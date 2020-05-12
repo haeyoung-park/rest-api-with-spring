bf8aac882363307bdb70a6daaa65043c7d88b87d
= <=>
2020-05-12 22:17:34 +0900

    project creation

---
+++ src/test/java/me/borebash/rest/ApplicationTest.java
@@ -0,0 +1,13 @@
+package me.borebash.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
