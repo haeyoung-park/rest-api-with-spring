7078811c9a8a7360b0db3b2bf2f01a7911bb6fd3
= <=>
2020-05-13 02:18:55 +0900

    polishing for test description

--- src/test/java/me/borebash/rest/events/EventControllerTests.java
+++ src/test/java/me/borebash/rest/events/EventControllerTests.java
@@ -27,6 +27,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import me.borebash.rest.common.TestDescription;
+
 // @WebMvcTest
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -43,6 +45,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성 검증")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder().name("SpringBoot").description("REST API Development with SpringBoot")
                 .beginEnrollmentDateTime(LocalDateTime.of(2020, 05, 12, 23, 57))
@@ -63,6 +66,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러 발생 검증")
     public void createEvent_BadRequest() throws Exception {
         Event event = Event.builder().id(100).name("SpringBoot").description("REST API Development with SpringBoot")
                 .beginEnrollmentDateTime(LocalDateTime.of(2020, 05, 12, 23, 57))
@@ -77,6 +81,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우 에러 검증")
     public void createEvent_BadRequest_EmptyInput() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -85,6 +90,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우 에러 검증 테스트")
     public void createEvent_BadRequest_WrongInput() throws JsonProcessingException, Exception {
         EventDto eventDto =  EventDto.builder()
             .name("SpringBoot")
@@ -99,7 +105,7 @@ public class EventControllerTests {
             .location("REST API Center")
             .build();
         
-        this.mockMvc.perform(post("/api/events")
+            this.mockMvc.perform(post("/api/events")
             .contentType(MediaType.APPLICATION_JSON)
             .content(this.objectMapper.writeValueAsString(eventDto))
         )
